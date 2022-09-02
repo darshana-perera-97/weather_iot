48df0cec021d1160db87e5858a5c3e7756125287
Darshana Perera <darshana.saluka.10@gmail.com>
2022-09-02 14:36:48 +0530

    react project created

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,27 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  FirebaseApp firebaseApp = await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,15 +26,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,68 +33,155 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  late DatabaseReference _dbref;
+  String databasejson = '';
+  var newAge;
+  var newAge2;
+  var status;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  _createDB() {
+    _dbref.child("profile").set(" my profile");
+    _dbref
+        .child("carprofile")
+        .set({'car1': "family car", 'car2': "company car"});
+  }
+
+  _readdb_onechild() {
+    _dbref
+        .child("customer1")
+        .child("age")
+        .once()
+        .then((DataSnapshot dataSnapshot) {
+      print(" read once - " + dataSnapshot.value.toString());
+      setState(() {
+        databasejson = dataSnapshot.value.toString();
+      });
     });
+  }
+
+  _realdb_once() {
+    _dbref.once().then((DataSnapshot dataSnapshot) {
+      print(" read once - " + dataSnapshot.value.toString());
+      setState(() {
+        databasejson = dataSnapshot.value.toString();
+      });
+    });
+  }
+
+  _updatevalue() {
+    _dbref.child("carprofile").update({"car2": "big company car"});
+  }
+
+  _delete() {
+    _dbref.child("profile").remove();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _dbref = FirebaseDatabase.instance.reference();
+    _readdb_onechild();
+    ageChange();
+    dataChange();
+    _createDB();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            buildText('Age is $databasejson'),
+            buildText('New Age is: $newAge'),
+            buildText('New Age is: $newAge2'),
+            buildText('New Status is: $status'),
+            StreamBuilder(
+              stream: _dbref.onValue,
+              builder: (context, AsyncSnapshot snap) {
+                if (snap.hasData &&
+                    !snap.hasError &&
+                    snap.data.snapshot.value != null) {
+                  Map data = snap.data.snapshot.value;
+                  List item = [];
+                  data.forEach(
+                      (index, data) => item.add({"key": index, ...data}));
+                  return Expanded(
+                    child: ListView.builder(
+                      itemCount: item.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text("Customer: ${item[index]['key']}"),
+                          subtitle: Text(
+                              'Age: ${item[index]['age'].toString()} \nStatus: ${item[index]['status']}'),
+                          isThreeLine: true,
+                        );
+                      },
+                    ),
+                  );
+                } else {
+                  return Center(child: Text("No data"));
+                }
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Text buildText(String s) {
+    return Text(
+      s,
+      style: const TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  void ageChange() {
+    /*
+       var subscription = FirebaseDatabase.instance
+      .reference()
+      .child('messages')
+      .Xxx
+      .listen((event) {
+        // process event
+      });
+
+      where Xxx is one of
+      onvalue
+      onChildAdded
+      onChildRemoved
+      onChildChanged
+
+      To end the subscription you can use
+      subscription.cancel();
+    */
+    _dbref.child('customer1').child('age').onValue.listen((Event event) {
+      int data = event.snapshot.value;
+      print('weight data: $data');
+      setState(() {
+        newAge = data;
+      });
+    });
+  }
+
+  void dataChange() {
+    _dbref.child('customer1').onValue.listen((event) {
+      print(event.snapshot.value.toString());
+      Map data = event.snapshot.value;
+      data.forEach((key, value) {
+        setState(() {
+          newAge2 = data['age'];
+          status = data['status'];
+        });
+      });
+    });
   }
 }
